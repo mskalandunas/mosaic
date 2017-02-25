73feaecbaccbdff4756d94a79559c49c8c4a832c
Mike Skalandunas <mskalandunas@gmail.com>
2017-02-25 01:46:00 -0800

    add explanations for all games

--- src/js/lib/tracks.js
+++ src/js/lib/tracks.js
@@ -9,27 +9,27 @@ const tracks = [
   {
     'src'         : '2',
     'title'       : '"The Ruined City of Quil"',
-    'description' : 'The ruins of an ancient temple.  Endless corridors, colossal antichambers, and history long lost.  This piece accompanies <a class="content--link" href="http://store.steampowered.com/app/264000/" target="_blank" title="Son of Nor">Son of Nor\'s</a> protagonist as they journey through the ruined temple of Taro, in the ancient city of Quil.  This was created mostly with strings from LA Scoring Strings, and live xiao.  I felt the long reverbs and lonely flute estbalished the feeling of being alone in such a long forgotten place quite perfectly.'
+    'description' : 'The ruins of an ancient temple.  Endless corridors, colossal antichambers, and history long lost.  This piece accompanies <em class="logo">Son of Nor\'s</em> protagonist as they journey through the ruined temple of Taro, in the ancient city of Quil.  This was created mostly with strings from LA Scoring Strings, and live xiao.  I felt the long reverbs and lonely flute estbalished the feeling of being alone in such a long forgotten place quite perfectly.'
   },
   {
     'src'         : '3',
     'title'       : '"Mable: The Journey"',
-    'description' : 'As an exercise to learn dynamic music layering in Wwise, I created this piece to accompany gameplay in <a class="content--link" href="https://triplevisiongames.itch.io/mable" target="_blank" title="Mable: The Journey">Mable: The Journey by Triple Vision Games</a>.  Its upbeat and exciting style was inspired by a fusion of Madeon and Disasterpeace.  In the end, I think it does a great job of adding to the fantasy atmosphere in a fast-paced retro world.'
+    'description' : 'As an exercise to learn dynamic music layering in Wwise, I created this piece to accompany gameplay in <em class="logo">Mable: The Journey by Triple Vision Games</em>.  Its upbeat and exciting style was inspired by a fusion of Madeon and Disasterpeace.  In the end, I think it does a great job of adding to the fantasy atmosphere in a fast-paced retro world.'
   },
   {
     'src'         : '4',
     'title'       : '"Breeze"',
-    'description' : 'Relaxing wind across Scottish cliffside plateaus, and the faint sound of chimes in the distance.  <em>Breeze</em> accompanies the player in their quest to herd sheep with a trusting (and unintentionally goofy) border collie.  Check out <a class="content--link" href="http://www.sheargames.com/" target="_blank" title="Sheargames">Bleat!</a> on mobile/tablet devices.'
+    'description' : 'Relaxing wind across Scottish cliffside plateaus, and the faint sound of chimes in the distance.  <em>Breeze</em> accompanies the player in their quest to herd sheep with a trusting (and unintentionally goofy) border collie.  Live guitar, <a class="content--link" href="https://soniccharge.com/synplant" target="_blank" title="Synplant">Synplant</a>, upright bass, instruments from the <a class="content--link" href="http://www.fbpsound.com/The-Toyshop-Sample-Library" target="_blank" title="Toyshop Sample Library">Toyshop Library</a>, and various synths.'
   },
   {
     'src'         : '5',
     'title'       : '"Giraffe Beats"',
-    'description' : 'Created for Nico Prinz\'s <a class="content--link" href="http://www.ohmygiraffe.com/" target="_blank" title="Oh My Giraffe">Oh My Giraffe</a>, an endless runner in Africa\'s savanna.  Because of <a class="content--link" href="http://www.ohmygiraffe.com/" target="_blank" title="Oh My Giraffe">Oh My Giraffe\'s</a> simple and colorful art style, I decided that a very MIDI/dry sampled approached would work best for the music.  I ended up using quite a bit of African percussion, some sampled and some live, as well as a dry Pan flute for the melody.'
+    'description' : 'Created for Nico Prinz\'s <em class="logo">Oh My Giraffe</em>, an endless runner in Africa\'s savanna.  Because of the simple and colorful art style, I decided that a very MIDI/dry sampled approached would work best for the music.  I ended up using quite a bit of African percussion, some sampled and some live, as well as a dry Pan flute for the melody.'
   },
   {
     'src'         : '6',
     'title'       : '"Winter"',
-    'description' : 'Created originally for Breadcrumbs Interactive\'s <a class="content--link" href="https://breadcrumbsinteractive.com/yaga/" target="_blank" title="Yaga">Yaga</a> as an unused demo.  This was based around Yaga\'s winter environment.  I wanted to create a sense of cold and loneliness, and I think the solemn progression along with the deep reverbs and bass does this quite well.  The flutes, especially the Irish penny-whistle, and the fluttering strings really lend themselves well to the frigid atmosphere.'
+    'description' : 'Created as a music redesign for <em class="logo">Northguard\'s</em> winter environment.  I wanted to create a sense of cold and loneliness, and I think the solemn progression along with the deep reverbs and bass does this quite well.  The flutes, especially the Irish penny-whistle, and the fluttering strings really lend themselves well to the frigid atmosphere and the search for warmth.'
   }
 ];
 
