de1d52a74805c1523d4580e0e8537c9c8db826f5
Mike Skalandunas <mskalandunas@gmail.com>
2017-07-16 21:52:22 -0700

    minify html

--- README.md
+++ README.md
@@ -3,9 +3,6 @@
 
 ####todo
 1.  Animate p/h6 in player
-2.  Get high pagespeed score
-3.  minify HTML
-4.  Featured Image
 5.  Figure out how to put minified css into head
 
 
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,9 +4,10 @@ import gulp from 'gulp';
 import cssNano from 'gulp-cssnano';
 import concatCss from 'gulp-concat-css';
 import webpack from 'webpack-stream';
+import htmlmin from 'gulp-htmlmin';
+import rmHtmlComments from 'gulp-remove-html-comments';
 
 const staticFiles = [
-  'src/**/*.html',
   'src/**/*.ico',
   'src/**/*.jpg',
   'src/**/*.mp3',
@@ -28,22 +29,9 @@ gulp.task('static:dev', () => {
   .pipe(gulp.dest(__dirname + '/public/'));
 });
 
-gulp.task('webpack:dev', () => {
-  return gulp.src('src/js/app.js')
-  .pipe(webpack({
-    output: {
-      filename: 'bundle.js'
-    }
-  }))
-  .pipe(gulp.dest('public/js/'));
-});
-
-gulp.task('css:dev', () => {
-  return gulp.src(styleSheets)
-  .pipe(concatCss('main.css'))
-  .pipe(cssNano())
-  .pipe(gulp.dest(__dirname + '/public/css'));
-});
+gulp.task('webpack:dev', () => gulp.src('src/js/app.js').pipe(webpack({output: {filename: 'bundle.js'}})).pipe(gulp.dest('public/js/')));
+gulp.task('css:dev', () => gulp.src(styleSheets).pipe(concatCss('main.css')).pipe(cssNano()).pipe(gulp.dest(__dirname + '/public/css')));
+gulp.task('html:dev', () => gulp.src('src/*.html').pipe(htmlmin({collapseWhitespace: true})).pipe(rmHtmlComments()).pipe(gulp.dest('public')));
 
 gulp.task('watch:build', () => {
   gulp.watch(staticFiles, ['static:dev']);
@@ -51,5 +39,5 @@ gulp.task('watch:build', () => {
   gulp.watch('src/**/*.js', ['webpack:dev']);
 });
 
-gulp.task('build', ['static:dev', 'webpack:dev', 'css:dev']);
+gulp.task('build', ['static:dev', 'webpack:dev', 'css:dev', 'html:dev']);
 gulp.task('default', ['build', 'watch:build']);
