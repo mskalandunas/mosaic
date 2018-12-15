0c080f6ad9a4b5a9de41a304989338b33a56b19f
Mike Skalandunas <mskalandunas@gmail.com>
2018-12-14 21:40:22 -0800

    [update]: update site images and clean up

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,39 +6,59 @@ import concatCss from 'gulp-concat-css';
 import webpack from 'webpack-stream';
 import htmlmin from 'gulp-htmlmin';
 import rmHtmlComments from 'gulp-remove-html-comments';
-import inject from 'gulp-inject';
-import {transform} from './lib/text-transform';
 
 const staticFiles = [
-  'src/**/*.ico',
-  'src/**/*.jpg',
-  'src/**/*.mp3',
-  'src/**/*.wav',
-  'src/**/*.svg',
-  'src/**/*.png',
-  'src/**/*.pdf'
+    'src/**/*.ico',
+    'src/**/*.jpg',
+    'src/**/*.mp3',
+    'src/**/*.wav',
+    'src/**/*.svg',
+    'src/**/*.png',
+    'src/**/*.pdf'
 ];
 
 const styleSheets = [
-  'src/css/normalize.css',
-  'src/css/skeleton.css',
-  'src/css/style.css',
-  'src/css/player.css'
+    'src/css/normalize.css',
+    'src/css/skeleton.css',
+    'src/css/style.css',
+    'src/css/player.css'
 ];
 
-gulp.task('static:dev', () => {
-  gulp.src(staticFiles)
-  .pipe(gulp.dest(__dirname + '/public/'));
-});
+gulp.task('static:dev', () =>
+    gulp.src(staticFiles)
+    .pipe(gulp.dest(__dirname + '/public/'))
+);
 
-gulp.task('webpack:dev', () => gulp.src('src/js/app.js').pipe(webpack({output: {filename: 'bundle.js'}})).pipe(gulp.dest('public/js/')));
-gulp.task('css:dev', () => gulp.src(styleSheets).pipe(concatCss('main.css')).pipe(cssNano()).pipe(gulp.dest(__dirname + '/public/css')));
-gulp.task('html:dev', () => gulp.src('src/*.html').pipe(htmlmin({collapseWhitespace: true})).pipe(rmHtmlComments()).pipe(gulp.dest('public')));
+gulp.task('webpack:dev', () =>
+    gulp.src('src/js/app.js')
+    .pipe(webpack({
+        output: {
+            filename: 'bundle.js'
+        }
+    }))
+    .pipe(gulp.dest('public/js/'))
+);
+
+gulp.task('css:dev', () =>
+    gulp.src(styleSheets)
+    .pipe(concatCss('main.css'))
+    .pipe(cssNano())
+    .pipe(gulp.dest(__dirname + '/public/css'))
+);
+
+gulp.task('html:dev', () =>
+    gulp.src('src/*.html')
+    .pipe(htmlmin({
+        collapseWhitespace: true
+    }))
+    .pipe(rmHtmlComments())
+    .pipe(gulp.dest('public'))
+);
 
 gulp.task('watch:build', () => {
-  gulp.watch(staticFiles, ['static:dev']);
-  gulp.watch(styleSheets, ['css:dev']);
-  gulp.watch('src/**/*.js', ['webpack:dev']);
+    gulp.watch(staticFiles, ['static:dev']);
+    gulp.watch(styleSheets, ['css:dev']);
+    gulp.watch('src/**/*.js', ['webpack:dev']);
 });
 
 gulp.task('build', ['static:dev', 'webpack:dev', 'css:dev', 'html:dev']);
--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,6 @@ require(__dirname + '/lib/email');
 require(__dirname + '/lib/scroll');
 
 const tracks = require(__dirname + '/lib/tracks');
-const Audio  = require(__dirname + '/lib/audio');
-const node   = document.querySelector('.mosaic-player');
+const Audio = require(__dirname + '/lib/audio');
+const node = document.querySelector('.mosaic-player');
 const mosaic = new Audio(node, tracks);
